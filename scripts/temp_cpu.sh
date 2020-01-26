5819f1d8fef11fb7bb10fd7b799a609f1980ae90
narugit <narusens@gmail.com>
2020-01-26 18:27:32 +0900

    initial commit

---
+++ scripts/temp_cpu.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# shellcheck source=helpers.sh
+source "$CURRENT_DIR/helpers.sh"
+
+print_cpu_temp() {
+  if command_exists "osx-cpu-temp"; then
+    local temp
+    temp=$(osx-cpu-temp | grep "[0-9]*\.[0-9]")
+    printf $temp
+  else
+    echo "no osx-cpu-temp found"
+  fi
+}
+
+main() {
+  print_cpu_temp
+  printf "°C"
+}
+main
