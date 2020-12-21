4ca296eb4f6cc84c38324cd195acec0b0dd667de
narugit <narusens@gmail.com>
2020-12-21 21:21:30 +0900

    Fix #2

--- scripts/temp_cpu.sh
+++ scripts/temp_cpu.sh
@@ -8,7 +8,7 @@ source "$CURRENT_DIR/helpers.sh"
 print_cpu_temp() {
   if command_exists "osx-cpu-temp"; then
     local temp
-    temp=$(osx-cpu-temp | grep "[0-9]*\.[0-9]")
+    temp=$(osx-cpu-temp | grep -o "[0-9]*\.[0-9]")
     printf $temp
   else
     echo "no osx-cpu-temp found"
